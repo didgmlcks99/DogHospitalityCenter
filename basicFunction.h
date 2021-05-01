23d1449e9a547cd83cbe397e2455333e8e4fbeed
didgmlcks99 <heechy06@naver.com>
2021-05-01 23:06:13 +0900

    basicFunction.h

---
+++ basicFunction.h
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct{
+	char name[60];
+    char type[60];
+    int year;
+    int month;
+	int weight;
+    int age;
+    char character[60];
+    bool vaccine;
+    bool neutralization;
+    char foundIn[60];
+} Dog;
+
+int addDog(Dog *d);
+void readData(Dog *d); 
+int updateData(Dog *d);   
+int deleteData(Dog *d);
+
+int selectMenu();
+void listData(Dog *d,int count);
+int selectDataNo(Dog *d, int count);
+
+void saveData(Dog *d, int count);
+int loadData(Dog *d);
