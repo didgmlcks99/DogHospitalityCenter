d74997b79728d957fe4e0674a5d1aaca277c6ca7
Joo <21800171@handong.edu>
2021-05-05 00:59:32 +0900

    selectMenu함수 구현 - 'basicFunction.c'

--- basicFunction.c
+++ basicFunction.c
@@ -18,7 +18,18 @@ int deleteData(Dog *d){
 
 
 int selectMenu(){
-    return 1;
+    int menu;
+    printf("\n*** 보호견 목록 ***\n");
+    printf("1. 조회\n");
+    printf("2. 추가\n");
+    printf("3. 수정\n");
+    printf("4. 삭제\n");
+    printf("5. 저장\n");
+    printf("6. 불러오기\n");
+    printf("0. 종료\n\n");
+    printf("=> 원하는 메뉴는? ");
+    scanf("%d", &menu);
+    return menu;
 }
 
 void listData(Dog *d,int count){
