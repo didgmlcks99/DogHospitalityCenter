7c23af5dc249bb50c7d277af8de2cf458371e351
didgmlcks99 <heechy06@naver.com>
2021-05-04 22:30:31 +0900

    minor change

--- dogCenter.c
+++ dogCenter.c
@@ -17,7 +17,7 @@ int main(void){
         if(menu == 0) break;
         if(menu == 1 || menu ==3 || menu == 4){	
 		    if (count==0){ 
-                printf("=>µ¥ÀÌÅÍ°¡ ¾ø½À´Ï´Ù!\n");
+                printf("=>ë°ì´í„°ê°€ ì—†ìŠµë‹ˆë‹¤!\n");
 			    continue;
 			}
 		}
@@ -29,7 +29,7 @@ int main(void){
         else if (menu == 3) {
             int no=selectDataNo(dlist, curcount);
             if(no==0){
-                printf("=>Ãë¼ÒµÊ!");
+                printf("=>ì·¨ì†Œë¨!");
                 continue;
             }
             updateData(&dlist[no-1]);
@@ -37,22 +37,22 @@ int main(void){
         else if (menu == 4) {
             int no=selectDataNo(dlist, curcount);
             if(no==0){
-                printf("=>Ãë¼ÒµÊ!");
+                printf("=>ì·¨ì†Œë¨!");
                 continue;
             }
             int deleteok;
-            printf("Á¤¸»·Î »èÁ¦ÇÏ½Ã°Ú½À´Ï±î?(»èÁ¦:1)");
+            printf("ì •ë§ë¡œ ì‚­ì œí•˜ì‹œê² ìŠµë‹ˆê¹Œ?(ì‚­ì œ:1)");
             scanf("%d",&deleteok);
             if(deleteok == 1){
                 if(deleteData(&dlist[no-1])) count --;       
            	 } 
         }
 	    // else if (menu == 5){
-		//     if (count==0) printf("µ¥ÀÌÅÍ°¡ ¾ø½À´Ï´Ù!\n");
+		//     if (count==0) printf("ë°ì´í„°ê°€ ì—†ìŠµë‹ˆë‹¤!\n");
 		//     else saveData(dlist,curcount);
 	    // }
 	}
 
-	printf("\nÁ¾·áµÊ!\n");
+	printf("\nì¢…ë£Œë¨!\n");
    	return 0;
 }
